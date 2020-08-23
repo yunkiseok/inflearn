796d758e52f34deb77cc633920c095e493f658bf
yunkiseok <boooker@naver.com>
2020-08-23 16:46:27 +0900

    정보보호

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+
+//application/x-www-form-urlencoded 이렇게 된 데이터를 분석해서 가져올 수 있게 함
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json 타입으로 된 데이터를 분석해서 가져올 수 있게 함
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://dbYun:yks9605+@cluster0.dhj3t.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
@@ -12,6 +23,20 @@ mongoose.connect('mongodb+srv://dbYun:yks9605+@cluster0.dhj3t.mongodb.net/<dbnam
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+    //회원가입할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    // 몽고db에서 오는 method인 save()
+    user.save((err, doc) => {
+      if(err) return res.json({ success: false, err})
+      return res.status(200).json({
+        success: true
+      })
+    })
 })
 
 app.listen(port, () => {
